fdebc1e10fa0f27e0be9a20e5bd61d4f46060eea
Oscar <operezcham@gameover.com.mx>
2017-03-25 01:15:56 -0600

    Create Coin.java

---
+++ Coin.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+public class Coin {
+	private long x;
+	private long m;
+	private final long p = 1301081L;
+	private final long q = 1299721L;
+	public Coin() {
+		m = p * q;
+		x0();
+	}
+	public boolean flip() {
+		return blumBlumShub();
+	}
+	private boolean blumBlumShub() {
+		x = (x * x) % m;
+		return parity(x);
+	}
+	private boolean parity(long n) {
+		BigInteger b = BigInteger.valueOf(n);
+		byte c = 0;
+		for (byte i = 0; i < 16; i++) {
+			if (b.testBit(i)) {
+				c++;
+			}
+		}
+		return c % 2 == 0;
+	}
+	private void x0() {
+		x = System.currentTimeMillis();
+		while (x % m == 0 && x < 2) {
+			x++;
+		}
+	}
+}
